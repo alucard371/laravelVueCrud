876a00c45ce865b213ce299096fdd614797e7d54
quantum design <tests8q@gmail.com>
2019-11-03 14:23:19 +0000

    server side; added controller migration model and route to laravel

---
+++ database/migrations/2019_11_03_141155_create_profile_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('basic-info');
+        Schema::dropIfExists('image');
+        Schema::create('profile', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('img_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
